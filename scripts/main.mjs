2a18f14896bc7075d246c3e8d1809d36a0515b44
jaywcjlove <398188662@qq.com>
2024-07-24 05:03:09 +0800

    feat: add scripts.

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,4 +1,7 @@
+import fs from 'fs-extra';
+import path from 'path';
 import SVGIcons2SVGFont from 'svgicons2svgfont';
+import svg2ttf from 'svg2ttf';
 
 /*
  * Filter svg files
@@ -20,16 +23,66 @@ export function filterSvgFiles(svgFolderPath) {
   return svgArr;
 }
 
-function svgToSVGFont(src) {
-  return new Promise((resolve, reject) => {
-    // init
-    const fontStream = new SVGIcons2SVGFont({
-      log: (message) => log.log(message),
-    });
+function charToSvgFontUnicode(char) {
+  // 获取字符的 Unicode 码点
+  let unicode = char.codePointAt(0);
+  // 转换为十六进制格式，并大写表示
+  let unicodeHex = unicode.toString(16).toUpperCase();
+  // 格式化为 SVG 字体使用的 Unicode 表示
+  let svgFontUnicode = `&#x${unicodeHex};`;
+  return svgFontUnicode;
+}
 
+function writeFontStream(svgPath, fontStream) {
+  // file name
+  let fileNmae = path.basename(svgPath, ".svg");
+  const glyph = fs.createReadStream(svgPath)
+  //const curUnicode = String.fromCharCode(fileNmae);
+  const curUnicode = charToSvgFontUnicode(fileNmae);
+  const unicode = [curUnicode];
+  console.log(`\n  ┌┈▶ ${fileNmae} ${unicode}`);
+  glyph.metadata = { unicode: [fileNmae] , name: fileNmae };
+  fontStream.write(glyph);
+}
+
+function svgToSVGFont(src = "svg", dist = "./fonts/pinyin.svg") {
+  return new Promise((resolve, reject) => {
+    const fontStream = new SVGIcons2SVGFont({
+      log: (message) => console.log(message),
+      fontName: "pinyin",
+      fontHeight: 1000,
+      normalize: true,
+    });
+    // Setting the font destination
+    fontStream.pipe(fs.createWriteStream(dist))
+      .on("finish", () => {
+        console.log(`SUCCESS SVG font successfully created!\n  ╰┈▶ ${dist}`)
+        resolve();
+      })
+      .on("error", (err) => {
+        if (err) {
+          reject(err);
+        }
+      });
+
+    filterSvgFiles(src).forEach((svgPath) => {
+      if (typeof svgPath !== 'string') return false;
+      writeFontStream(svgPath, fontStream);
+    });
+    // Do not forget to end the stream
+    fontStream.end();
   });
 }
 
+function svgFontToTTF(src = "./fonts/pinyin.svg", dist = "./fonts/pinyin.ttf") {
+  const svgFont = fs.readFileSync(src, "utf8");
+  const ttf = svg2ttf(svgFont, {});
+  const ttfBuf = Buffer.from(ttf.buffer);
+  fs.writeFileSync(dist, ttfBuf);
+  console.log(`SUCCESS TTF font successfully created!\n  ╰┈▶ ${dist}`);
+}
+
 ;(async () => {
-  await svgToSVGFont("./svg");
+  await svgToSVGFont("./svg", "./fonts/pinyin.svg");
+  svgFontToTTF("./fonts/pinyin.svg", "./fonts/pinyin.ttf");
 })()
